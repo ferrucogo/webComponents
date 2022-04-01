3e08810a3c316ab695fc171080ac37f554830f20
ferrucogo <andreaferrucogo@gmail.com>
2022-04-01 09:04:50 +0200

    First Release

---
+++ main.js
@@ -0,0 +1,30 @@
+import confetti from 'canvas-confetti';
+
+function replaceSelected(user) {
+    const selectedContainer = document.getElementById('selected-user');
+    selectedContainer.innerHTML = '';
+    const pippoTag = document.createElement('pippo-tag');
+    pippoTag.setAttribute('pippo-user', JSON.stringify(user));
+    selectedContainer.appendChild(pippoTag);
+}
+
+document.addEventListener('user-selected', (e) => replaceSelected(e.detail));
+
+const users = [
+    { name: 'andrea', mail: 'popolo@grr.la' },
+    { name: 'francesca', mail: 'paperina@grr.la' },
+    { name: 'simone', mail: 'pluto@grr.la' },
+    { name: 'marco', mail: 'pippo@grr.la' },
+];
+
+users.forEach((user) => {
+    const pippoTag = document.createElement('pippo-tag');
+    pippoTag.setAttribute('pippo-user', JSON.stringify(user));
+    pippoTag.setAttribute('has-button', 'true');
+    document.body.appendChild(pippoTag);
+});
+
+confetti.create(document.getElementById('canvas'), {
+    resize: true,
+    useWorker: true,
+})({ particleCount: 200, spread: 200 });
---
+++ super-header.js
@@ -0,0 +1,42 @@
+class SuperHeader extends HTMLHeadingElement {
+    // eslint-disable-next-line no-useless-constructor
+    constructor() {
+        super();
+    }
+
+    attributeChangedCallback() {
+        this.getAttributes();
+        this.initTag();
+    }
+
+    static get observedAttributes() { return ['user-count']; }
+
+    getAttributes() {
+        if (this.getAttribute('user-count')) {
+            this.userCount = parseInt(this.getAttribute('user-count'), 10);
+        }
+    }
+
+    initTag() {
+        this.style.background = 'green';
+
+        let container;
+        if (document.getElementById('additional-content')) {
+            container = document.getElementById('additional-content');
+            container.innerHTML = '';
+        } else {
+            container = document.createElement('div');
+            container.id = 'additional-content';
+        }
+
+        const span = document.createElement('span');
+        const node = document.createTextNode(`count: ${this.userCount}`);
+        span.appendChild(node);
+
+        container.appendChild(span);
+
+        this.appendChild(container);
+    }
+}
+
+window.customElements.define('super-header', SuperHeader, { extends: 'h1' });
